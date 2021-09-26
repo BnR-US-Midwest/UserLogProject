53ae135c7ea50366d2a606b9955ac162b895873d
Tyler Matijevich <tyler.matijevich@br-automation.com>
2021-09-26 11:40:11 -0500

    Update the sample programs with usage examples, and the sample loop

--- Logical/String/Main.c
+++ Logical/String/Main.c
@@ -14,6 +14,8 @@ void _INIT ProgramInit(void)
 	memset(str1 + sizeof(str1) - 1, 0, 1);
 	memcpy(mem1, str1, sizeof(mem1));
 	memcpy(mem2, str2, sizeof(mem2));
+	
+	LogMessage(USERLOG_SEVERITY_SUCCESS, 1234, "Hello World!");
 }
 
 void _CYCLIC ProgramCyclic(void)
