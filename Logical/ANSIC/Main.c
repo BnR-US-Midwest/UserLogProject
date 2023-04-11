40a5a75c38b3c01f5a95c31003d16ea75d1cdf0c
Tyler Matijevich <tyler.matijevich@br-automation.com>
2023-04-11 16:32:10 -0500

    New library tag version, update function examples

--- Logical/ANSIC/Main.c
+++ Logical/ANSIC/Main.c
@@ -1,62 +1,38 @@
 
-#include <bur/plctypes.h>
-#include <string.h>
-#include <stdint.h>
-#include <stdbool.h>
-
 #ifdef _DEFAULT_INCLUDES
 	#include <AsDefault.h>
 #endif
 
-#define MAX(x,y) (((x) > (y)) ? (x) : (y))
-#define MIN(x,y) (((x) < (y)) ? (x) : (y))
+#include <string.h>
+#include <stdbool.h>
 
 void _INIT ProgramInit(void)
 {
-	strcpy(Logbook, "$arlogusr");
-	Severity = USERLOG_SEVERITY_DEBUG;
-	Facility = 2;
-	Code = 1234;
-	pObject = (uint32_t)Object;
-	strcpy(Message, "Hello World!");
-	pMessage = (uint32_t)Message;
-	pArguments = (uint32_t)&Arguments;
-	Level = USERLOG_SEVERITY_SUCCESS;
-	
-	UserLogCreate("Test", 200000);
-	
 	UserLogBasic(USERLOG_SEVERITY_SUCCESS, 1234, "Hello World!");
 	
-	Arguments.i[0] = Code;
-	UserLogAdvanced(USERLOG_SEVERITY_INFORMATION, Code, "My code is %i", &Arguments);
+	FormatValues.b[0] = false;
+	FormatValues.f[0] = 23.1;
+	FormatValues.i[0] = -1070586087;
+	strcpy(FormatValues.s[0], "Starting");
+	UserLogAdvanced(USERLOG_SEVERITY_INFORMATION, 567, "Error=%b, Temperature=%f C, StatusID=%i, State=%s", &FormatValues);
 	
-	LoopMax = 5;
+	UserLogCreate("Custom", 200000);
+	UserLogCustom("Custom", USERLOG_SEVERITY_WARNING, 25, 789, 0, NULL, "This message is written to the Custom logbook", NULL);
 	
 	UserLogEventText("$arlogusr", USERLOG_EXAMPLE_TEXT_ID, 0, NULL, "This is a description of the log record", NULL);
+	
+	UserLogSetSeverityLevel(USERLOG_SEVERITY_WARNING);
+	UserLogBasic(USERLOG_SEVERITY_DEBUG, 1, "This is a debug message that will be suppressed");
+	UserLogBasic(USERLOG_SEVERITY_WARNING, 2, "This is a warning message that will not be suppressed");
+	
+	Severity = UserLogGetSeverity(USERLOG_EXAMPLE_TEXT_ID);
+	Facility = UserLogGetFacility(USERLOG_EXAMPLE_TEXT_ID);
+	Code = UserLogGetCode(USERLOG_EXAMPLE_TEXT_ID);
 }
 
 void _CYCLIC ProgramCyclic(void)
 {
-	if(Test) {
-		Test = false;
-		ReturnValue = UserLogCustom(Logbook, Severity, Facility, Code, Origin, (char*)pObject, (char*)pMessage, (UserLogFormatType*)pArguments);
-	}
-	UserLogSetSeverityLevel(Level);
 	
-	if (TestLoop) 
-	{
-		TestLoop = false;
-		for (Index = 0; Index < LoopMax; Index++) 
-		{
-			Arguments.i[0] = Index + 1;
-			UserLogCustom(Logbook, Severity, Facility, Code, Origin, (char*)pObject, (char*)pMessage, (UserLogFormatType*)pArguments);
-		}
-	}
-	
-	EventID = ArEventLogMakeEventID(MIN(arEVENTLOG_SEVERITY_ERROR, MAX(arEVENTLOG_SEVERITY_SUCCESS, Severity)), Facility, Code);
-	GetSeverity = UserLogGetSeverity(EventID);
-	GetFacility = UserLogGetFacility(EventID);
-	GetCode = UserLogGetCode(EventID);
 }
 
 void _EXIT ProgramExit(void)
